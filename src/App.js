cd9ac848dc05e7579f3025998c22c6da3eaba1b5
Kylie-Lang1 <kylielang@pursuit.org>
2023-04-27 13:22:02 -0400

    testing connection to backend

--- src/App.js
+++ src/App.js
@@ -1,22 +1,26 @@
-import logo from './logo.svg';
+import { useEffect, useState } from "react";
+import axios from "axios";
 import './App.css';
 
+const API = process.env.REACT_APP_BASE_URL;
+
 function App() {
+  const [ message, setMessage ] = useState()
+
+  useEffect(() => {
+    axios
+      .get(`${API}`)
+      .then((res) => {
+        setMessage(res.data.message)
+        console.log(message)
+      })
+      .catch((c) => console.warn("catch, c"));
+  }, []);
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>{message}</h1>
       </header>
     </div>
   );
