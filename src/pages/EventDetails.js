fdb223fb4e6f3be0979d25f8751c6810ad2b09cd
DanielR2105 <rodriguez.daniel2105@gmail.com>
2023-05-08 11:52:26 -0400

    started skeleton for events

---
+++ src/pages/EventDetails.js
@@ -0,0 +1,20 @@
+// Will add an axious call to fetch the information of a specific event
+// If the map ends up being its own component we can just import it here
+
+export default function EventDetails() {
+  return (
+    <div>
+      <h1>Title</h1>
+      <h2>Date</h2>
+      <h2>Age Restrictions</h2>
+      <h2>Categories</h2>
+      <h2>Summary</h2>
+      <div>
+        <h2>Attendees(Number)</h2>
+      </div>
+      <div>
+        <h2>Comments</h2>
+      </div>
+    </div>
+  );
+}
---
+++ src/pages/ShowEvents.js
@@ -0,0 +1,22 @@
+import SearchEvents from "../components/SearchEvents";
+import EventCard from "../components/EventCard";
+
+// This will be the page on the wireframe that displays all of the events. The search bar, event cards, the map plugin, and the categories
+//  will be their own components.
+// Should the sort button also be its own component?
+// Since the categories will double as clickable filters should they even be their own components?
+// There will also be an axios call fetching all events once the backend and frontend are connected
+// Create event button will ink to new event page once routes are setup
+export default function ShowEvents() {
+  return (
+    <div>
+      <SearchEvents />
+      <button>Create Event</button>
+      <button>Sort by</button>
+      <EventCard />
+      <EventCard />
+      <EventCard />
+      <EventCard />
+    </div>
+  );
+}
