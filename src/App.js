4d53456777470929e6d86b64e01cfa4e6c02831d
Kylie-Lang1 <kylielang@pursuit.org>
2023-05-08 11:45:27 -0400

    updated App.js

--- src/App.js
+++ src/App.js
@@ -5,30 +5,14 @@ import SignUp from "./Pages/SignUp";
 import Login from "./Pages/Login";
 import './App.css';
 
-const API = process.env.REACT_APP_BASE_URL;
-
 function App() {
-  const [ message, setMessage ] = useState()
-
-  useEffect(() => {
-    axios
-      .get(`${API}`)
-      .then((res) => {
-        setMessage(res.data.message)
-        console.log(message)
-      })
-      .catch((c) => console.warn("catch, c"));
-  }, []);
-
   return (
-    
     <div className="App">
       <Router>
         <Routes>
           {/* <Route path='/' element={} /> */}
           <Route path='/login' element={<Login />}/>
           <Route path='/signup' element={<SignUp />}/>
-
         </Routes>
       </Router>
     </div>
