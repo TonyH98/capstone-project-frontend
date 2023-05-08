0ab74952df2462f81930744845c282f1f9e6649d
Kylie-Lang1 <kylielang@pursuit.org>
2023-05-08 12:13:06 -0400

    styling for login

--- src/App.js
+++ src/App.js
@@ -1,9 +1,6 @@
-import { useEffect, useState } from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import SignUp from "./Pages/SignUp";
 import Login from "./Pages/Login";
-import NavBar from "./components/NavBar";
-import Footer from "./components/Footer";
 import NavBar from "./components/NavBar";
 import Footer from "./components/Footer";
 import './App.css';
--- src/Pages/Login.js
+++ src/Pages/Login.js
@@ -1,29 +1,45 @@
+import { Link } from 'react-router-dom'
+
 function Login() {
     return (
         <div>
-            <form className="w-96 py-20 bg-indigo-200 m-auto">
-                <button>X</button>
+            <form className="w-96 py-10 bg-indigo-200 m-auto relative">
+                <button className="absolute right-3 top-2 text-xl text-red-800">X</button>
+                <p className='pb-5 text-md'>
+                    Welcome back! Input login info below
+                </p>
                 <label htmlFor="username">
                     <input 
                         type='text' 
                         name='usernname' 
                         placeholder="Username" 
                         required 
-                        className="mb-5 rounded pl-3"
+                        className="mb-5 rounded pl-3 block m-auto"
                     />
                 </label>
-                <br/>
+
                 <label htmlFor="password">
                     <input 
                         type='text' 
                         name='password' 
-                        placeholder="Password" 
+                        placeholder="Password"
                         required 
-                        className="rounded pl-3"
+                        className="rounded pl-3 block m-auto"
                     />
                 </label>
-                <br/>
-                <button type='submit' className="bg-indigo-500 px-3 text-white mt-5 rounded">Login</button>
+
+                <button 
+                    type='submit' 
+                    className="bg-indigo-500 px-3 text-white mt-5 rounded hover:bg-indigo-400"
+                >
+                    Login
+                </button>
+                <p className='text-sm mt-5'>
+                    Don't have an account yet?
+                </p>
+                <Link to='/signup' className='block mt-1 underline text-blue-500 hover:text-white'>
+                    Sign Up
+                </Link>
             </form>
         </div>
     );
