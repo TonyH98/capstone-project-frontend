5ba14a57ec46f4bbb88af51481bff6b3d1012f41
TonyH98 <tonyhoang@pursuit.org>
2023-05-12 14:50:19 -0400

    The form is able to story multiple categories

--- src/pages/NewEvent.js
+++ src/pages/NewEvent.js
@@ -1,24 +1,65 @@
-import { useState } from "react";
+import axios from "axios";
+import { useState , useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 // The page is only missing the axios call to post an event to the backend
 
+
+const API = process.env.REACT_APP_API_URL
 export default function NewEvent() {
+
+
   let navigate = useNavigate();
-  const [event, setEvent] = useState({
+
+  const [events, setEvent] = useState({
     title: "",
-    categories: "",
-    age: 18, // default value for age is 18
+    categoryIds: [],
+    age: 18,
     location: "",
     max: "", // this value sets the max attendees allowed
-    date: "",
-    time: "",
+    date_event: "",
+    start_time: "",
+    end_time: "",
     summary: "",
+    address: "",
+    location_image: "",
+    creator_id: "" //id of the user
   });
-  const categoryOptions = ["Option 1", "Option 2", "Option 3"]; // This is a placeholder until we have categories in the backend
+  
+let [category , setCategory] = useState([])
 
-  const handleTextChange = (event) => {
-    setEvent({ ...event, [event.target.id]: event.target.value });
-  };
+
+useEffect(() => {
+  axios
+  .get(`${API}/category`)
+  .then((res) => {
+    setCategory(res.data)
+  })
+  .catch((error) => {
+    console.log(error)
+  })
+}, [])
+
+
+const handleAdd = (newEvent) => {
+
+  axios
+  .post(`${API}/events` , newEvent)
+
+}
+
+const handleTextChange = (event) => {
+  if (event.target.id === "categoryIds") {
+    const { value } = event.target;
+
+    setEvent((prevEvent) => ({
+      ...prevEvent,
+      categoryIds: [...prevEvent.categoryIds, value],
+    }));
+  } else {
+    setEvent({ ...events, [event.target.id]: event.target.value });
+  }
+};
+
 
   const handleAgeChange = (event) => {
     setEvent({ ...event, age: parseInt(event.target.value) });
@@ -26,8 +67,11 @@ export default function NewEvent() {
 
   function handleSubmit(event) {
     event.preventDefault();
-    // the axios call to post will go here
+    handleAdd(events)
   }
+
+
+console.log(events)
 
   return (
     <div>
@@ -36,12 +80,12 @@ export default function NewEvent() {
         <input type="text" id="title" onChange={handleTextChange} />
         <br />
 
-        <label htmlFor="categories">Categories</label>
-        <select id="categories" onChange={handleTextChange}>
+        <label htmlFor="categoryIds">Categories</label>
+        <select id="categoryIds" onChange={handleTextChange}>
           <option value="">Select a category</option>
-          {categoryOptions.map((option) => (
-            <option key={option} value={option}>
-              {option}
+          {category.map((option) => (
+            <option key={option.id} value={option.name}>
+              {option.name}
             </option>
           ))}
         </select>
@@ -59,14 +103,22 @@ export default function NewEvent() {
         <input type="number" id="max" onChange={handleTextChange} />
         <br />
 
-        <label htmlFor="date">Date</label>
-        <input type="date" id="date" onChange={handleTextChange} />
+        <label htmlFor="date_event">Date</label>
+        <input type="date" id="date_event" onChange={handleTextChange} />
         <br />
 
-        <label htmlFor="time">Time</label>
-        <input type="time" id="time" onChange={handleTextChange} />
+        <label htmlFor="start_time">Start Time</label>
+        <input type="time" id="start_time" onChange={handleTextChange} />
         <br />
+        
+        <label htmlFor="end_time">End Time</label>
+        <input type="time" id="end_time" onChange={handleTextChange} />
+        <br />
+        
+        <label htmlFor="address">Address</label>
+        <input type="text" id="address" onChange={handleTextChange}/>
 
+        <br/>
         <label htmlFor="summary">Summary</label>
         <textarea id="summary" onChange={handleTextChange} />
         <br />
