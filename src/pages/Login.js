40d4483b7464aa86b537f6840a789967713f3b71
DanielR2105 <rodriguez.daniel2105@gmail.com>
2023-05-16 11:08:54 -0400

    working on user page

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -34,70 +34,69 @@ function Login() {
     setLogin({ ...login, [e.target.id]: e.target.value });
   };
 
+  return (
+    <div className="flex flex-col justify-center items-center p-6">
+      <form className="sm:w-96 bg-white shadow-md p-4 rounded-lg flex flex-col items-center justify-center">
+        <button className="relative left-[50%] font-bold text-lg">X</button>
+        <div>
+          <p className="text-lg font-bold py-4">
+            Welcome back! Input login info below
+          </p>
+          <label htmlFor="username">
+            <input
+              type="text"
+              id="username"
+              name="usernname"
+              placeholder="Username"
+              required
+              onChange={handleTextChange}
+              className="rounded w-[85%] bg-transparent appearance-none focus:outline-none"
+            />
+          </label>
 
-    return (
-        <div className='flex flex-col justify-center items-center p-6'>
-            <form className="sm:w-96 bg-white shadow-md p-4 rounded-lg flex flex-col items-center justify-center">
-                <button className="relative left-[50%] font-bold text-lg">X</button>
-                <div>
-                <p className='text-lg font-bold py-4'>
-                    Welcome back! Input login info below
-                </p>
-                <label htmlFor="username">
-                    <input 
-                        type='text' 
-                        id='username'
-                        name='usernname' 
-                        placeholder="Username" 
-                        required 
-                        onChange={handleTextChange}
-                        className="rounded w-[85%] bg-transparent appearance-none focus:outline-none"
-                    />
-                </label>
-
-                <label htmlFor="password">
-                    <input 
-                        type={ showPassword ? 'text' : 'password' } 
-                        id='password'
-                        name='password' 
-                        placeholder="Password"
-                        required 
-                        onChange={handleTextChange}
-                        className="my-3 rounded w-[85%]"
-                    />
-                    { 
-                    showPassword ? 
-                        <button 
-                            onClick={() => setShowPassword(!showPassword)}    
-                            className="px-1 font-semibold"
-                        >
-                            Hide
-                        </button> : 
-                        <button 
-                                onClick={() => setShowPassword(!showPassword)} 
-                                className="px-1 font-semibold"
-                            >
-                                Show
-                        </button> 
-                }
-                </label>
-                </div>
-                <button 
-                    type='submit' 
-                    className="bg-cyan-400 border border-cyan-400 text-white hover:text-cyan-400 rounded-md hover:bg-transparent hover:border px-2 py-1 font-bold"
-                >
-                    Login
-                </button>
-                <p className='text-sm mt-5'>
-                    Don't have an account yet?
-                </p>
-                <Link to='/signup' className='block mt-1 underline underline-offset-2 font-semibold text-blue-500 hover:text-cyan-400'>
-                    Sign Up
-                </Link>
-            </form>
+          <label htmlFor="password">
+            <input
+              type={showPassword ? "text" : "password"}
+              id="password"
+              name="password"
+              placeholder="Password"
+              required
+              onChange={handleTextChange}
+              className="my-3 rounded w-[85%]"
+            />
+            {showPassword ? (
+              <button
+                onClick={() => setShowPassword(!showPassword)}
+                className="px-1 font-semibold"
+              >
+                Hide
+              </button>
+            ) : (
+              <button
+                onClick={() => setShowPassword(!showPassword)}
+                className="px-1 font-semibold"
+              >
+                Show
+              </button>
+            )}
+          </label>
         </div>
-    );
-=======
+        <button
+          type="submit"
+          className="bg-cyan-400 border border-cyan-400 text-white hover:text-cyan-400 rounded-md hover:bg-transparent hover:border px-2 py-1 font-bold"
+        >
+          Login
+        </button>
+        <p className="text-sm mt-5">Don't have an account yet?</p>
+        <Link
+          to="/signup"
+          className="block mt-1 underline underline-offset-2 font-semibold text-blue-500 hover:text-cyan-400"
+        >
+          Sign Up
+        </Link>
+      </form>
+    </div>
+  );
   const handleSubmit = (event) => {
     event.preventDefault();
     logIn(event);
@@ -168,7 +167,6 @@ function Login() {
       </form>
     </div>
   );
-
 }
 
 export default Login;
