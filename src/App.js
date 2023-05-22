ba2ec98b8785ea8e5d20791dae83ec1d7dade52e
DanielR2105 <rodriguez.daniel2105@gmail.com>
2023-05-22 12:22:42 -0400

    testing userProvider

--- src/App.js
+++ src/App.js
@@ -18,6 +18,7 @@ import EventDetails from "./pages/EventDetails";
 import NewEvent from "./pages/NewEvent";
 import Map from "./components/Map";
 import useLocalStorage from "./hooks/useLocalStorage";
+import { UserProvider } from "./contexts/UserProvider";
 const API = process.env.REACT_APP_API_URL;
 
 function App() {
@@ -58,33 +59,35 @@ function App() {
       {/* useContext files can be pass here to allow all components to have access to global data */}
       {/* let's say we have a FriendsProvider file in our contexts folder, we can pass it here and the MESSAGES, USER_PROFILE, and EVENTS components will all have access to it from here, no need to pass props or creating multiple state. */}
       {/* <FriendsProvider> */}
-      <Router>
-        <NavBar />
-        <main className="h-[100%]">
-          <Routes>
-            <Route path="/" element={<Landing />} />
-            <Route path="/login" element={<Login />} />
-            <Route path="/signup" element={<SignUp />} />
-            {/* 
+      <UserProvider>
+        <Router>
+          <NavBar />
+          <main className="h-[100%]">
+            <Routes>
+              <Route path="/" element={<Landing />} />
+              <Route path="/login" element={<Login />} />
+              <Route path="/signup" element={<SignUp />} />
+              {/* 
               Comment in when useParams is set up and remove UserProfile below
               <Route path='/profile/:id' element={<UserProfile />} /> 
               */}
-            <Route path="/profile/:username" element={<UserProfile />} />
-            <Route path="/devs" element={<Devs />} />
-            <Route path="/events" element={<ShowEvents />} />
-            <Route path="/events/new" element={<NewEvent />} />
-            {/* 
+              <Route path="/profile/:username" element={<UserProfile />} />
+              <Route path="/devs" element={<Devs />} />
+              <Route path="/events" element={<ShowEvents />} />
+              <Route path="/events/new" element={<NewEvent />} />
+              {/* 
               Comment in when useParams is set up and remove EventDetails below
               <Route path='/events/:id' element={<EventDetails />} /> 
               */}
-            <Route path="/events/:id" element={<EventDetails />} />
-            <Route path="/map" element={<Map />} />
-          </Routes>
-        </main>
-        {/* <Footer/> */}
-      </Router>
+              <Route path="/events/:id" element={<EventDetails />} />
+              <Route path="/map" element={<Map />} />
+            </Routes>
+          </main>
+          {/* <Footer/> */}
+        </Router>
 
-      {/* </FriendsProvider> */}
+        {/* </FriendsProvider> */}
+      </UserProvider>
     </div>
   );
 }
---
+++ src/contexts/UserProvider.js
@@ -0,0 +1,14 @@
+import React, { useContext } from "react";
+import useLocalStorage from "../hooks/useLocalStorage";
+
+const UserContext = React.createContext();
+
+export function useUser() {
+  return useContext(UserContext);
+}
+
+export function UserProvider({ children }) {
+  const [user, setUser] = useLocalStorage("user", {});
+}
+
+return <UserContext.Provider value={user}>{children}</UserContext.Provider>;
