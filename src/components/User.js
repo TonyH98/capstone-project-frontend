b20962b9fc10dc133f91cc4124919859313dddcc
TonyH98 <tonyhoang@pursuit.org>
2023-05-23 14:02:21 -0400

    Generic Profile Image Added to the Front End

--- src/components/User.js
+++ src/components/User.js
@@ -1,6 +1,9 @@
 import React from 'react'
 
 export default function User({user}) {
+
+console.log(user.profile_img)
+
   return (
     <div className='bg-cyan-100 shadow-lg p-4 rounded-md flex'>
         <img src={user.profile_img} alt={`${user.first_name} profile image`}></img>
