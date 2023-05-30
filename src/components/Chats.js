a7dcc3eaead63f4a8e6dab1748e3c0fb23ffb77b
Fadila-Ali <fadilaali@pursuit.org>
2023-05-30 10:28:42 -0400

    I can send a message

--- src/components/Chats.js
+++ src/components/Chats.js
@@ -6,95 +6,101 @@ import useLocalStorage from "../hooks/useLocalStorage";
 let socket;
 const API = process.env.REACT_APP_API_URL;
 
-function Chats({ loggedin, setLoggedin, user, firebaseId }) {
-  // Before Login
+function Chats({ loggedin, setLoggedin, user, setUser, firebaseId }) {
   const [room, setRoom] = useState("");
-
-  // After Login
   const [message, setMessage] = useState("");
-  const [messageList, setMessageList] = useLocalStorage('messageList', []);
-  const [users, setUsers] = useLocalStorage('users', [])
-  const [searchUser, setSearchUser] = useState("")
+  const [messageList, setMessageList] = useLocalStorage("messageList", []);
+  const [users, setUsers] = useLocalStorage("users", []);
+  const [searchUser, setSearchUser] = useState("");
 
   useEffect(() => {
     socket = io(API);
+    socket.on("receive_message", (data) => {
+      setMessageList((prevList) => [...prevList, data]);
+    });
   }, [API]);
 
-  useEffect(() => {
-    socket.on("receive_message", (data) => {
-      setMessageList([...messageList, data]);
-    });
-  });
   const connectToRoom = () => {
     setLoggedin(true);
     socket.emit("join_room", room);
   };
 
-  const sendMessage = async () => {
-    let messageContent = {
-      room: room,
+  const sendMessage = () => {
+    console.log(user);
+    console.log(user.first_name);
+    const messageContent = {
+      room,
       content: {
-        author: firebaseId,
-        message: message,
+        author: user.first_name,
+        message,
       },
     };
 
-    console.log(`User: ${JSON.stringify(user)}`);
-    console.log(`Message: ${message}`);
-    console.log(`Room: ${room}`)
-
-    await socket.emit("send_message", messageContent);
-    setMessageList([...messageList, messageContent.content]);
+    socket.emit("send_message", messageContent);
+    setMessageList((prevList) => [...prevList, messageContent.content]);
     setMessage("");
   };
 
+  useEffect(() => {
+    axios
+      .get(`${API}/users`)
+      .then((res) => {
+        setUsers(res.data);
+      })
+      .catch((error) => {
+        console.error("Error fetching users:", error);
+      });
+  }, []);
 
-     // useEffect makes get request for all Users
-     useEffect(() => {
-        axios
-          .get(`${API}/users`)
-          .then((res) => {
-            setUsers(res.data);
-            console.log(res.data);
-        })
-        .catch((c) => console.warn("catch, c"));
-    }, []);
+  const filterUsers = users.filter((user) => {
+    const { first_name, last_name, username } = user;
+    const usernameMatch = username
+      .toLowerCase()
+      .includes(searchUser.toLowerCase());
+    const firstNameMatch = first_name
+      .toLowerCase()
+      .includes(searchUser.toLowerCase());
+    const lastNameMatch = last_name
+      .toLowerCase()
+      .includes(searchUser.toLowerCase());
 
-    const filterUsers = users.filter((user) => {
-      const { first_name, last_name, username } = user;
-      const usernameMatch = username.toLocaleLowerCase().includes(searchUser.toLocaleLowerCase());
-      const firstNameMatch = first_name.toLocaleLowerCase().includes(searchUser.toLocaleLowerCase());
-      const lastNameMatch = last_name.toLocaleLowerCase().includes(searchUser.toLocaleLowerCase());
-      console.log(usernameMatch)
+    return usernameMatch || firstNameMatch || lastNameMatch;
+  });
 
-      return usernameMatch || firstNameMatch || lastNameMatch;
-    })
+  const handleRecipientSelection = (recipientId) => {
+    setRoom(`${user.id}-${recipientId}`);
+  };
 
   return (
     <div className="p-4">
       {!loggedin ? (
         <div className="text-center">
-          <div className='search-bar'>
-            <label htmlFor='search'>Search User: </label>
+          <div className="search-bar">
+            <label htmlFor="search">Search User: </label>
             <input
               type="text"
-              placeholder='username'
-              id='search'
+              placeholder="username"
+              id="search"
               value={searchUser}
               onChange={(e) => setSearchUser(e.target.value)}
-        />
-      </div>
-          <div>No Conversations yet, search a user to start a conversation!</div>
+            />
+          </div>
+          <div>
+            No conversations yet, search for a user to start a conversation!
+          </div>
         </div>
       ) : (
         <div className="flex gap-6 divide-x-4">
-          <div className="text-2xl text-cyan-400 font-semibold p-4">Conversations</div>
+          <div className="text-2xl text-cyan-400 font-semibold p-4">
+            Conversations
+          </div>
           <div className="flex flex-col p-4">
             {messageList.map((val, key) => {
               return (
                 <div
                   className="messageContainer"
-                  id={val.author === firebaseId ? "You" : user.name}
+                  id={val.author === user.first_name ? "You" : user.first_name}
+                  key={key}
                 >
                   <div className="messageIndividual">
                     {val.author}: {val.message}
@@ -102,19 +108,29 @@ function Chats({ loggedin, setLoggedin, user, firebaseId }) {
                 </div>
               );
             })}
-          <div className="absolute bottom-0 p-4">
-            <input
-              type="text"
-              placeholder="Message..."
-              onChange={(e) => {
-                setMessage(e.target.value);
-              }}
-            />
-            <button onClick={sendMessage}>Send</button>
-          </div>
+            <div className="absolute bottom-0 p-4">
+              <input
+                type="text"
+                placeholder="Message..."
+                value={message}
+                onChange={(e) => setMessage(e.target.value)}
+              />
+              <button onClick={sendMessage}>Send</button>
+            </div>
           </div>
         </div>
       )}
+      <div>
+        <h2>Users</h2>
+        {filterUsers.map((user) => (
+          <div
+            key={user.id}
+            onClick={() => handleRecipientSelection(user.id)}
+          >
+            {user.first_name}{" "}{user.last_name}
+          </div>
+        ))}
+      </div>
     </div>
   );
 }
