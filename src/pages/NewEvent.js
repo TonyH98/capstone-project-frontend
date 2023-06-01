a010f37768b67ff01148126cb4d98d54437467cb
Fadila-Ali <fadilaali@pursuit.org>
2023-06-01 13:17:59 -0400

    new form done

--- src/pages/NewEvent.js
+++ src/pages/NewEvent.js
@@ -3,8 +3,8 @@
 
 // Create new event form that posts a new event to the events table
 import axios from "axios";
-import { useState, useEffect } from "react";
-import { useNavigate } from "react-router-dom";
+import { useState , useEffect } from "react";
+import { useNavigate, Link } from "react-router-dom";
 import Geocode from "react-geocode";
 import GoogleMap from "../components/Map";
 import { useUser } from "../contexts/UserProvider";
@@ -19,6 +19,21 @@ export default function NewEvent () {
   const [ coordinates, setCoordinates ] = useState({})
   const [ addressIsVerified, setAddressIsVerified ] = useState(false)
   const [ isValid, setIsValid ] = useState(false)
+
+  // this is to make the form a 2 step
+  const [formStep, setFormStep] = useState(0);
+
+  // moves user to the next step of the form
+  const nextForm = (e) => {
+    e.preventDefault();
+    setFormStep((currentStep) => currentStep + 1);
+  };
+
+  // moves user to the previous step of the form
+  const prevForm = () => {
+    setFormStep((currentStep) => currentStep - 1);
+  };
+
 
   const { user } = useUser();
   const  [users, setUsers] = useState({})
@@ -304,52 +319,156 @@ const verifyAddress = () => {
   }
   
   return (
-    <div className="flex justify-center items-center p-4 flex gap-20">
-      {
-        events?.location_image ? (
-          <img 
-            src={events?.location_image} 
-            alt='event photo' 
-            className="max-h-96 max-w-96"
+    <div className="flex items-center justify-center p-4 gap-20">
+      <form onSubmit={handleSubmit} className="bg-white shadow-md rounded-md px-10 pt-6 pb-8 mb-4 w-3/8">
+        { formStep === 0 && (
+          <section className="">
+            <div className="mb-3">
+          <label htmlFor="title" className="block text-gray-700 text-sm font-bold mb-1">Title</label>
+          <input 
+            type="text" 
+            id="title" 
+            value={events.title} 
+            onChange={handleTextChange} 
+            required
+            className="shadow bg-transparent appearance-none border w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline rounded-md"
           />
-        ) : (
-          <div className="bg-gray-200 ">
-            <p className="h-96 w-96 text-center pt-[170px]">Preview event image</p>
-          </div>
-        )
-      }
-      <form onSubmit={handleSubmit}>
-        <label htmlFor="title">Title</label>
-        <input 
-          type="text" 
-          id="title" 
-          value={events.title} 
-          onChange={handleTextChange} 
-          required
-        />
-        <br />
-
-        <label htmlFor="categoryIds">Categories</label>
-        <select 
-          id="categoryIds" 
-          value={events.categoryIds.length > 0 ? events.categoryIds[0] : ""}
-          onChange={handleTextChange} 
-          required
-        >
-          <option value="">
-            Select a category
-          </option>
-          {
-            category.map((option) => (
-              <option key={option.id} value={option.name}>
-                {option.name}
-              </option>
-            ))
-          }
-        </select>
-        <br />
+        </div>
         
+        <div className="mb-3">
+          <label htmlFor="location" className="block text-gray-700 text-sm font-bold mb-2">
+            Location
+          </label>
+          <input 
+            type="text" 
+            id="location" 
+            value={events.location}
+            onChange={handleTextChange} 
+            required
+            className="shadow bg-transparent appearance-none border w-full  py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline rounded-md"
+          />
+        </div>
+        <div className="mb-3">
+          <label htmlFor="address" className="block text-gray-700 text-sm font-bold mb-2">
+            Address
+          </label>
+          <input 
+            type="text" 
+            id="address" 
+            value={events.address}
+            onChange={handleTextChange} 
+            required
+            className="shadow bg-transparent appearance-none border w-full  py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline rounded-md"
+          />
+          <button
+            type='button'
+            className="underline block mt-2 text-purple-500 text-sm"
+            onClick={verifyAddress}
+          >
+            Verify address
+          </button>
+          {
+            addressError && <p style={{color:"red"}}>{addressError}</p>
+          }
+        </div>
+        <div className="flex justify-between gap-2">
+          <div className="mb-3">
+            <label htmlFor="date_event" className="block text-gray-700 text-sm font-bold mb-2">
+              Date
+            </label>
+            <input 
+              type="date" 
+              id="date_event" 
+              value={events.date_event}
+              onChange={handleTextChange} 
+              required
+              className="shadow bg-transparent appearance-none border py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline rounded-md"
+            />
+          </div>
         {
+          dateError && <p style={{color:"red"}}>{dateError}</p>
+        }
+        <div className="flex gap-2">
+          <div className="mb-3">
+            <label htmlFor="start_time" className="block text-gray-700 text-sm font-bold mb-2">
+              Start Time
+            </label>
+            <input
+              type="time" 
+              id="start_time" 
+              value={events.start_time}
+              onChange={handleTextChange} 
+              required
+              className="shadow bg-transparent appearance-none border  py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline rounded-md"
+            />
+         </div>
+          <div className="mb-3">
+            <label htmlFor="end_time" className="block text-gray-700 text-sm font-bold mb-2">
+              End Time
+            </label>
+            <input 
+            type="time" 
+            id="end_time" 
+            value={events.end_time}
+            onChange={handleTextChange}
+            required
+            className="shadow bg-transparent appearance-none border  py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline rounded-md"
+            />
+          </div>
+        </div>
+        </div>
+        <div className="mb-3">
+            <label htmlFor="max" className="block text-gray-700 text-sm font-bold mb-2">
+              Max Participants
+            </label>
+            <input 
+              type="number" 
+              id="max_people" 
+              onChange={handleTextChange} 
+              value={events.max_people}
+              required
+              className="shadow bg-transparent appearance-none border py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline rounded-md"
+            />
+          </div>
+          {
+            maxError && <p style={{color:"red"}}>{maxError}</p>
+          }
+        <div className="flex justify-evenly pt-4">
+        {formStep < 1 ? <button onClick={nextForm} className="block border border-cyan-400 bg-cyan-400 hover:bg-purple-400
+        hover:border-purple-400 text-slace-900 hover:text-slate-100 uppercase text-sm font-bold py-2 px-4 rounded-md">Next</button> : ""}
+        <Link to={`/events`}>
+            <button className="block border border-gray-500 hover:bg-[#f6854b] hover:border-[#f6854b] text-slace-900 hover:text-slate-100 uppercase text-sm font-bold p-2 rounded-md">
+              Cancel
+            </button>
+          </Link>
+        </div>
+          </section>
+        )}
+        { formStep === 1 && (
+          <section className="w-[450px] py-6">
+        <div className="mb-3 flex">
+          <div className="mr-2">
+            <label htmlFor="categoryIds" className="block text-gray-700 text-sm font-bold mb-1">Categories</label>
+            <select 
+              id="categoryIds" 
+              value={events.categoryIds.length > 0 ? events.categoryIds[0] : ""}
+              onChange={handleTextChange} 
+              required
+              className="shadow bg-transparent appearance-none border w-full py-2 pl-3 mr-2 text-gray-700 leading-tight focus:outline-none focus:shadow-outline rounded-md"
+            >
+            <option value="">
+              Select a category
+            </option>
+            {
+              category.map((option) => (
+                <option key={option.id} value={option.name}>
+                  {option.name}
+                </option>
+              ))
+            }
+          </select>
+          </div>
+          {
           events.categoryIds.length > 0 ? (
               <div className="category-container">
                 {
@@ -369,10 +488,11 @@ const verifyAddress = () => {
                 }
               </div>
             ) : null
-        }
-          <br/>
-
-          <label htmlFor="age_restriction">
+          }
+        </div>
+        <div className="mb-3 flex">
+          <div>
+          <label htmlFor="age_restriction" className="block text-gray-700 text-sm font-bold mb-2">
             Age Restriction
           </label>
             <select 
@@ -380,31 +500,36 @@ const verifyAddress = () => {
               value={events.age_restriction}
               onChange={handleTextChange} 
               required
+              className="shadow bg-transparent appearance-none border py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline rounded-md"
             >
               <option value="">Select Option</option>
               <option value={true}>True</option>
               <option value={false}>False</option>
             </select>
+          </div>
             {
               events.age_restriction ? (
-                <div>
-                  <label htmlFor="age_min">
-                    Minimum Age
+                <div className="flex">
+                  <div className="">
+                  <label htmlFor="age_min" className="block text-gray-700 text-sm font-bold mb-2">
+                    Min Age
                   </label>
                   <input 
                     type="number" 
                     id="age_min" 
                     value={events.age_min}
                     onChange={handleTextChange} 
+                    className="shadow bg-transparent appearance-none border py-2 px-3 mx-2 w-20 text-gray-700 leading-tight focus:outline-none focus:shadow-outline rounded-md"
                   />
-                  <br />
+                  </div>
                   {
                     ageError && <p style={{color:"red"}}>{ageError}</p>
                   }
                   {
                     minAge && <p style={{color:"red"}}>{minAge}</p>
                   }
-                  <label htmlFor="age_max">
+                  <div>
+                  <label htmlFor="age_max" className="block text-gray-700 text-sm font-bold mb-2">
                     Max Age
                   </label>
                   <input 
@@ -412,147 +537,90 @@ const verifyAddress = () => {
                     id="age_max" 
                     value={events.age_max}
                     onChange={handleTextChange} 
+                    className="shadow bg-transparent appearance-none border py-2 px-3 mx-2 w-20 text-gray-700 leading-tight focus:outline-none focus:shadow-outline rounded-md"
                   />
+                  </div>
                 </div>
               ): null
             }
-            <br/>
-        <br />
+          </div>
         {
           ageError && <p style={{color:"red"}}>{ageError}</p>
         }
-
-        <label htmlFor="location">
-          Location
-        </label>
-        <input 
-          type="text" 
-          id="location" 
-          value={events.location}
-          onChange={handleTextChange} 
-          required
-        />
-        <br />
-
-        <label htmlFor="max">
-          Max Participants
-        </label>
-        <input 
-          type="number" 
-          id="max_people" 
-          onChange={handleTextChange} 
-          value={events.max_people}
-          required
-        />
-        <br/>
-        {
-          maxError && <p style={{color:"red"}}>{maxError}</p>
-        }
-
-        <label htmlFor="date_event">
-          Date
-        </label>
-        <input 
-          type="date" 
-          id="date_event" 
-          value={events.date_event}
-          onChange={handleTextChange} 
-          required
-        />
-        <br/>
-        {
-          dateError && <p style={{color:"red"}}>{dateError}</p>
-        }
-
-        <label htmlFor="start_time">
-          Start Time
-        </label>
-        <input
-          type="time" 
-          id="start_time" 
-          value={events.start_time}
-          onChange={handleTextChange} 
-          required
-        />
-        <br/>
         
-        <label htmlFor="end_time">
-          End Time
-        </label>
-        <input 
-          type="time" 
-          id="end_time" 
-          value={events.end_time}
-          onChange={handleTextChange}
-          required
-        />
-        <br/>
-        
-        <label htmlFor="address">
-          Address
-        </label>
-        <input 
-          type="text" 
-          id="address" 
-          value={events.address}
-          onChange={handleTextChange} 
-          required
-        />
-
-        <button
-          type='button'
-          className="underline pl-3"
-          onClick={verifyAddress}
-        >
-          Verify address
-        </button>
-        <br/>
-        
-        {
-          addressError && <p style={{color:"red"}}>{addressError}</p>
-        }
-
-        <label htmlFor="location_image">
-          Image
-        </label>
-        <input 
-          type="text" 
-          id="location_image" 
-          value={events.location_image}
-          onChange={handleTextChange} 
-        />
-        <br/>
-
-        <label htmlFor="summary">
-          Summary
-        </label>
-        <textarea 
-          type="text" 
-          id="summary" 
-          value={events.summary}
-          onChange={handleTextChange}
-          required
-        />
-        <br/>
-
-        <input type="submit" />
+        <div className="mb-3">
+          <label htmlFor="location_image" className="block text-gray-700 text-sm font-bold mb-2">
+            Image
+          </label>
+          <input 
+            type="text" 
+            id="location_image" 
+            value={events.location_image}
+            onChange={handleTextChange} 
+            className="shadow bg-transparent appearance-none border w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline rounded-md"
+          />
+        </div>
+        <div className="mb-3">
+          <label htmlFor="summary" className="block text-gray-700 text-sm font-bold mb-2">
+            Summary
+          </label>
+          <textarea 
+            type="text" 
+            id="summary" 
+            value={events.summary}
+            onChange={handleTextChange}
+            required
+            className="shadow bg-transparent appearance-none border w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline rounded-md"
+          />
+        </div>
+        <div className="flex justify-evenly font-semibold">
+        {formStep > 0 ? <button onClick={prevForm} className="block border border-gray-500 hover:bg-cyan-400 hover:border-cyan-400 text-slace-900 hover:text-slate-100 uppercase text-sm font-bold p-2 rounded-md">previous</button> : ""}
+          <input type="submit" className="bg-cyan-400 hover:bg-purple-500 text-slate-100 uppercase text-sm font-bold py-2 px-4 rounded-md" />
+          <Link to={`/events`}>
+            <button className="block border border-gray-500 hover:bg-[#f6854b] hover:border-[#f6854b] text-slace-900 hover:text-slate-100 uppercase text-sm font-bold p-2 rounded-md">
+              Cancel
+            </button>
+          </Link>
+        </div>
+          </section>
+        )}
       </form>
-      {
-        addressIsVerified ? (
+      { formStep === 0 && (
+        <> {
+        (!addressError && !!events?.address) ? (
           <GoogleMap 
             mapWidth="300px"
             mapHeight="300px"
             mapLat={coordinates?.latitude}
             mapLng={coordinates?.longitude}
           />
-          ) : (
-          <div className="">
+        ) : (
+          <div className="w-[300px] h-[300px]">
             <p className="w-[300px] h-[300px] bg-gray-200 text-center pt-[125px] m-auto">
               Please verify a valid address
             </p>
           </div>
+        )}
+        </>
+      )
+      }
+      { formStep === 1 && (
+        <>
+        {
+        events?.location_image ? (
+          <img 
+            src={events?.location_image} 
+            alt='event photo' 
+            className="max-h-[300px] max-w-[300px]"
+          />
+        ) : (
+          <div className="bg-gray-200 w-[300px] h-[300px] flex justify-center items-center">
+            <p className="w-96 text-center">Preview event image</p>
+          </div>
         )
       }
+        </>
+      )}
     </div>
   );
 }
