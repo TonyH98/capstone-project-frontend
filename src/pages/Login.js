b41f2db26f3114b145052ee6f24536d8e4984a63
DanielR2105 <rodriguez.daniel2105@gmail.com>
2023-06-06 22:51:45 -0400

    Login now redirects with username, component done

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -51,8 +51,7 @@ function Login() {
   const handleSubmit = (event) => {
     event.preventDefault();
     logIn(event);
-    // navigate(`/profile/${loggedInUser.username}`);
-    navigate("/");
+    navigate(`/profile/${loggedInUser.username}`);
   };
 
   return (
