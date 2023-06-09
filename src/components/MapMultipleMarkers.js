9e99b350ea4fa779e967822fd513f832371d1339
Kylie-Lang1 <kylielang@pursuit.org>
2023-06-09 12:17:57 -0400

    adding multi-ple markers to maps partially working

--- src/components/MapMultipleMarkers.js
+++ src/components/MapMultipleMarkers.js
@@ -54,9 +54,6 @@ function MultipleMarkers ({ filterEvents, events }) {
     })
   }, [filterEvents])
 
-  console.log(filterEvents)
-  console.log(markers)
-
   const Markers = () =>
     markers.map((marker) => (
         <>
