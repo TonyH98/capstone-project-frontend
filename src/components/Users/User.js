dfa813043b449fad53bf7b50ab3100b756eaaa0b
TonyH98 <tonyhoang@pursuit.org>
2023-07-23 13:30:36 -0400

    User page media query finished

--- src/components/Users/User.js
+++ src/components/Users/User.js
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from "react";
 import { Link, useNavigate } from "react-router-dom";
 import { useUser } from "../../contexts/UserProvider";
 import axios from "axios";
-
+import "./User.css"
 
 const API = process.env.REACT_APP_API_URL;
 
@@ -46,27 +46,27 @@ export default function User({ users }) {
   }, [loggedInUser?.id]);
 
   return (
-    <div className="lg:bg-cyan-100 lg:shadow-lg lg:h-[150px] lg:rounded-md lg:flex lg:justify-between">
-      <div className="lg:flex">
+    <div className="users-card lg:bg-cyan-100 lg:shadow-lg lg:h-[150px] lg:rounded-md lg:flex lg:justify-between">
+      <div className="users-card-info-container lg:flex">
         <img
           src={users?.profile_img}
           alt={`${users?.first_name} profile image`}
-          className="lg:h-[150px] lg:w-40 lg:rounded-l lg:object-cover"
+          className="users-card-profile-image lg:h-[150px] lg:w-40 lg:rounded-l lg:object-cover"
         ></img>
-        <div className="lg:p-2">
-          <h3 className="lg:font-semibold">
+        <div className=" users-card-name-container lg:p-2">
+          <h3 className=" users-card-fullname lg:font-semibold">
             {users?.first_name} {users?.last_name}
           </h3>
           <Link to={`/profile/${users?.username}`}>
-            <h3 className="lg:text-cyan-400 lg:font-bold lg:text-sm">@{users?.username}</h3>
+            <h3 className="users-card-username lg:text-cyan-400 lg:font-bold lg:text-sm">@{users?.username}</h3>
           </Link>
         </div>
       </div>
-      <div className="lg:flex lg:justify-center lg:items-center lg:gap-4 lg:p-2">
+      <div className="users-card-buttons-container lg:flex lg:justify-center lg:items-center lg:gap-4 lg:p-2">
       {loggedInUser?.id === users?.id ? null :
         <button
         onClick={() => navigate("/chats")}
-        className="lg:border-2 lg:border-cyan-400 lg:hover:bg-cyan-400 lg:px-2 lg:py-1.5 lg:rounded-md"
+        className="users-card-message-button lg:border-2 lg:border-cyan-400 lg:hover:bg-cyan-400 lg:px-2 lg:py-1.5 lg:rounded-md"
       >
         <Link to="/chats">Message</Link>
       </button>}
@@ -75,14 +75,14 @@ export default function User({ users }) {
         <span className="lg:border-2 lg:border-cyan-400 lg:hover:bg-cyan-400 lg:px-2 lg:py-1.5 lg:rounded-md">Already Friends</span>
       ) : request && sendRequest ? (
         <span
-          onClick={() => setSendRequest(false)}
-          className="lg:border-2 lg:border-cyan-400 lg:bg-cyan-400 lg:px-2 lg:py-1.5 lg:w-[140px] lg:rounded-md"
+        
+          className="users-card-request-sent lg:border-2 lg:border-cyan-400 lg:bg-cyan-400 lg:px-2 lg:py-1.5 lg:w-[140px] lg:rounded-md"
         >
-          Cancel Request
+        Request Sent
         </span>
       ) : (
         <button
-          className="lg:border-2 lg:border-cyan-400 lg:hover:bg-cyan-400 lg:px-2 lg:w-[140px] lg:py-1.5 lg:rounded-md"
+          className="users-card-friends-request-button lg:border-2 lg:border-cyan-400 lg:hover:bg-cyan-400 lg:px-2 lg:w-[140px] lg:py-1.5 lg:rounded-md"
           onClick={sendFriendRequest}
         >
           Friend Request
